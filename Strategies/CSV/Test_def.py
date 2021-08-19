45e4386a9bd2cf22122ef36205c6f051f2985bb2
jun-bitacademy <jeongjiyoon93@gmail.com>
2021-08-19 18:11:26 +0900

    8.19.21 - def_Custom_sharpe.py, def_Custom_eff.py 추가

--- Strategies/CSV/Test_def.py
+++ Strategies/CSV/Test_def.py
@@ -1,7 +1,7 @@
 ################### 함수들 테스트 하는 곳 #####################
 
 # from def_InstantRaise_sharpe import InstanceRaise_sharpe
-from def_InstantRaise_eff import InstanceRaise_eff
+# from def_InstantRaise_eff import InstanceRaise_eff
 # from def_Momentum1mo_sharpe import Momentum1mo_sharpe
 # from def_Momentum1mo_eff import Momentum1mo_eff
 # from def_Momentum3mos_sharpe import Momentum3mos_sharpe
@@ -10,9 +10,11 @@ from def_InstantRaise_eff import InstanceRaise_eff
 # from def_Updown_eff import Updown_eff
 # from def_Dual_sharpe import Dual_sharpe
 # from def_Dual_eff import Dual_eff
+# from def_Custom_sharpe import Custom_sharpe
+from def_Custom_eff import Custom_eff
 
 # InstanceRaise_sharpe()
-InstanceRaise_eff()
+# InstanceRaise_eff()
 # Momentum1mo_sharpe()
 # Momentum1mo_eff()
 # Momentum3mos_sharpe()
@@ -21,3 +23,5 @@ InstanceRaise_eff()
 # Updown_eff()
 # Dual_sharpe()
 # Dual_eff()
+# Custom_sharpe()
+Custom_eff()
